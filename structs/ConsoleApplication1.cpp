4dcd34c88ef2fb2a311b65a71e3352ce885794ae
kech45 <keremsunayhalil@gmail.com>
2023-01-04 02:11:04 +0200

    Moved ConsoleApplication1.cpp into structs

---
+++ structs/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+struct worker 
+{
+	char name[1024];
+	int salary;
+	worker& chief;
+
+};
+
+struct team
+{
+	char teamName[1024];
+	worker workers[100];
+	std::size_t amountOfWorkers = sizeof(workers) / sizeof(workers[0]);
+
+};
+
+struct company
+{
+	char companyName[1024];
+	team teams[50];
+	std::size_t amountOfTeams = sizeof(teams) / sizeof(teams[0]);
+};
+
+bool isWorkerInCompany()
+{
+
+}
+
+
+
+int main()
+{
+
+
+}
